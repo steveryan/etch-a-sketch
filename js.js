755bba74353493dba1ad4352a523446f998c0fda
Steve Ryan <steveryan@new-host.home>
2016-07-26 19:13:25 -0400

    work in progress. Squares appearing but not in a grid.

--- js.js
+++ js.js
@@ -1,2 +1,19 @@
-var $container = $('.container')
-$container.append('<p>Testing a paragraph element</p>')
+$(document).ready(function(){
+	var $container = $('.container');
+	var size = prompt('How many rows?');
+	var gridSize = 800/size;
+	var cell = gridSize+"px";
+	$container.append(cell)
+	/*$('h1').click(function(){
+		$(this).append('<p>Testing</p>');
+	}); */
+	for (i = 0; i < (size*size); i++) {
+		$container.append("<div class='block'></div>");
+	}
+	$('.block').css('width', gridSize);
+	$('.block').css('height', gridSize);
+	$('.block').css('border-color', 'black'); 
+	$('.block').css('border-weight', '1px'); 
+	$('.block').css('border-style', 'solid'); 
+      
+});
