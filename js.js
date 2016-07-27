5ab0326cfef408ff5c0f6f27460d4925d3b5ef2c
Steve Ryan <steveryan@new-host.home>
2016-07-26 21:28:57 -0400

    Grid appears properly, but there's a constant 3px border. I have no idea why

--- js.js
+++ js.js
@@ -1,21 +1,17 @@
 $(document).ready(function(){
 	var $container = $('.container');
 	var size = prompt('How many rows?');
-	var gridSize = 774/size;
-	var cell = gridSize+"px";
-	//$container.append(cell)
-	/*$('h1').click(function(){
-		$(this).append('<p>Testing</p>');
-	}); */
+	var gridSize = (800-(size*6))/size;
+	
 	for (i = 0; i < (size*size); i++) {
 		$container.append("<div class='block'></div>");
 	}
-	$('.block').css('width', gridSize);
-	$('.block').css('height', gridSize);
-	$('.block').css('border-color', 'black'); 
-	$('.block').css('border-weight', '1px'); 
-	$('.block').css('border-style', 'solid'); 
-	$('.block').css('display', 'inline-block'); 
-
+		$('.block').css('width', gridSize);
+		$('.block').css('height', gridSize);
+		$('.block').css('border-color', 'black'); 
+		$('.block').css('border-weight', '2px'); 
+		$('.block').css('border-style', 'solid'); 
+		$('.block').css('display', 'inline-block'); 
+		
       
 });
