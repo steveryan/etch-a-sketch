8c70f202c84c9a380753fc017aaab527a769590e
Steve Ryan <steveryan@new-host.home>
2016-07-28 21:48:08 -0400

    Working completely, need to functionized, since I've repeated a lot of code.

--- js.js
+++ js.js
@@ -17,5 +17,27 @@ $('.block').hover(function(){
 	$(this).css('background-color', 'blue'); 
 
 });
+
+$('h1').click(function(){
+	$('.block').remove();
+
+	size=prompt('How many rows?');
+	gridSize = (800-(size*2))/size;
+	for (i = 0; i < (size*size); i++) {
+		$container.append("<div class='block'> </div>");
+	};
+		$('.block').css('width', gridSize);
+		$('.block').css('height', gridSize);
+		$('.block').css('border-color', 'black'); 
+		$('.block').css('border-width', '1px'); 
+		$('.block').css('border-style', 'solid'); 
+		$('.block').css('display', 'inline-block');
+
+$('.block').hover(function(){
+	$(this).css('background-color', 'blue'); 
+
+});
+});
+
       
 });
