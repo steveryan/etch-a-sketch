3c990f5cf17ca6b33857d1887b53ef54534a1912
Steve Ryan <steveryan@new-host.home>
2016-07-28 21:27:44 -0400

    blue squares work now. Still need a function to clear/redraw grid

--- js.js
+++ js.js
@@ -1,17 +1,21 @@
 $(document).ready(function(){
 	var $container = $('.container');
 	var size = prompt('How many rows?');
-	var gridSize = (800-(size*6))/size;
+	var gridSize = (800-(size*2))/size;
 	
 	for (i = 0; i < (size*size); i++) {
-		$container.append("<div class='block'></div>");
+		$container.append("<div class='block'> </div>");
 	}
 		$('.block').css('width', gridSize);
 		$('.block').css('height', gridSize);
 		$('.block').css('border-color', 'black'); 
-		$('.block').css('border-weight', '2px'); 
+		$('.block').css('border-width', '1px'); 
 		$('.block').css('border-style', 'solid'); 
 		$('.block').css('display', 'inline-block'); 
 		
+$('.block').hover(function(){
+	$(this).css('background-color', 'blue'); 
+
+});
       
 });
